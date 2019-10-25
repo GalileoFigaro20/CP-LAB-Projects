214ce159da15ff676d65815f5897194883cf1482
GalileoFigaro20 <ivan.alexandru20@yahoo.com>
2019-10-25 11:13:32 +0300

    Commit#2 : added the second method for the problem with the BITSHIFT '<<' operator;

--- 2ndPowerNumber/main.c
+++ 2ndPowerNumber/main.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
 
 int main() {
+
     int number;
     scanf("%d", &number);
+    printf("Method no. 1 : \n");
+
+    ///Method no.1 : AND operator
     if(!(number & (number-1)))
+        printf("The number %d is a power of 2", number);
+    else printf("The number %d is not a power of 2", number);
+
+    int i = 1;
+    while(i < number)
+    {
+        i = i <<1;
+    }
+
+    printf("\n\n");
+    printf("Method no. 2 :  \n");
+
+    ///Method no.2 : BITSHIFT operator
+    if(i == number)
         printf("The number %d is a power of 2", number);
     else printf("The number %d is not a power of 2", number);
 
